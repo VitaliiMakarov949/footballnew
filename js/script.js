eaa1f3a3e3cf3296d3767bbe566b30f41bbdf49d
VitaliiMakarov949 <vitaliimakarov854@gmail.com>
2025-08-28 17:08:33 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,14 @@
+burger.onclick = () => {
+    navigation.style.left = 0;
+    black_bg.style.display = "block"
+  }
+  
+  cancel.onclick = () => {
+    menu.style.left = "-100%"
+    black_bg.style.display = "none"
+  }
+  
+  black_bg.onclick = () => {
+    menu.style.left = "-100%"
+    black_bg.style.display = "none"
+  }
